78424b64c8f82a70b76bff4b229d3b132c90cd34
hyerinhahm <hhln0828@gmail.com>
2024-09-01 13:34:55 +0900

    14400 solved

--- Project1/BruteForce_14400.cpp
+++ Project1/BruteForce_14400.cpp
@@ -1,6 +1,10 @@
 // Brute Force > 편의점2 (14400번)
+// x모음에서 최소랑 y 모음에서 최소이면 됨
+// 즉 x랑 y랑 따로 하면 됨 -> 독립 시행 
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 
@@ -13,14 +17,33 @@ int main()
 	int n;
 	cin >> n;
 
-	pair<int,int> pos[100000];
+	vector<int> posX(n);
+	vector<int> posY(n);
 
 	for (int i = 0; i < n; i++)
 	{
 		int y, x;
 		cin >> x >> y;
-		pos[i] = {x, y};
+		posX[i] = x;
+		posY[i] = y;
 	}
 
-	cout << "출력";
+	// x,y 각각 따로 정렬
+	sort(posX.begin(), posX.end());
+	sort(posY.begin(), posY.end());
+
+	int midX = 0, midY = 0;
+	midX = n / 2;
+	midY = n / 2;
+
+	long long sumX = 0, sumY = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sumX += abs(posX[midX] - posX[i]);
+		sumY += abs(posY[midY]- posY[i]);
+	}
+
+	cout << sumX + sumY;
+
+	
 }
