50277159383deda7dba8ad834b19da44bd599c3d
petemus <93657221+petemus@users.noreply.github.com>
2024-10-22 17:51:37 +0900

    .

---
+++ Project1/Problem1.cpp
@@ -0,0 +1,43 @@
+// 최대 점수 뽑기 문제
+// 쉬운 DP 문제
+
+#include <iostream>
+#include <vector>
+#include <memory.h>
+using namespace std;
+
+#define MAX_INDEX 100001
+
+// memo 최대 10만 
+int memo0[MAX_INDEX];		// 자기 자신 미포함 memo
+int memo1[MAX_INDEX];		// 자기 자신 포함 memo
+
+int GetMaxScore(vector<int>& array)
+{
+
+	// memo 초기값
+	memo0[0] = 0;
+	memo1[0] = array[0];
+
+	// 
+	for (int i = 1; i < array.size(); i++)
+	{	
+		// 자기 미포함이면 이전 index에서는 포함해야함
+		memo0[i] = memo1[i - 1];
+		// 자기 자신 포함
+		memo1[i] = max(memo0[i - 1], memo1[i - 1]) + array[i];
+	}
+
+	return max(memo0[array.size() - 1], memo1[array.size() - 1]);
+
+}
+
+
+int main()
+{
+
+	vector<int> array = {-3, 2, 4, -1, -2, -5};
+
+	cout << GetMaxScore(array);
+
+}
---
+++ Project1/Problem2.cpp
@@ -0,0 +1,17 @@
+// 천공카드 문제
+// 문자열 들어올때마다 이전 문자열 확인해주면 -> 최대 N^2이 나올 것 같아서 그렇게 풀면 안될듯
+
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+}
