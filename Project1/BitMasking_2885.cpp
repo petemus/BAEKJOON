e8dcf43b9b9661ba338a3d426da3bb4388f539b7
hyerinhahm <hhln0828@gmail.com>
2025-04-02 01:15:28 +0900

    bitmasking 2885 solved

---
+++ Project1/BitMasking_2885.cpp
@@ -0,0 +1,70 @@
+// Bitmasking > 초콜릿 식사(2885번)
+// 풀이시간: 3~40m
+// !! 엣지케이스 잘 확인하자
+
+
+// point 1 > 비트마스킹 -> k는 초콜릿들로 구성 (ex : 6 -> 110, 12 -> 1100)
+//			=> 초콜릿의 쪼개지는 수는 1의 개수와 0오른쪽에 1일 경우그 1도 더함 
+// Point 2 > 가장 작은 초콜릿의 크기는 어떻게 구하는가 -> k의 끝을 << 로 한칸 이동한 것과 같음 
+//		-> 이 경우는 그 오른쪽이 다 0이 아닌경우에만 가능 
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int k;
+	cin >> k;
+
+	int chocoSize = 0;
+	int divideCnt = 0;
+	
+	// 초콜릿 크기
+	int digit = 0;
+	for(int i = 31; i >= 0; --i)
+	{
+		// 이때의 자리의 다음 
+		if (k & (1 << i))
+		{
+			if (k == (1 << i))
+			{
+				// 바로 종료해도 됨
+				cout << (1 << i) << " " << 0;
+				return 0;
+			}
+			else
+			{
+				chocoSize = (1 << i + 1);
+			}
+			digit = i;
+			break;
+		}
+	}
+
+	// 최소 쪼개야 하는 수
+	int zeroCnt = 0;
+	for (int i = digit; i >= 0; --i)
+	{
+		if (k & (1 << i))
+		{
+			// 1마주치면 1개 증가, 0이 오른쪽에 있으면 그 수만큼 증가 
+			divideCnt += (zeroCnt) + 1;
+			zeroCnt = 0;
+		}
+		else
+		{
+			// 0이면 zeroCnt + 1
+			++zeroCnt;
+		}
+	}
+
+
+	// *output
+	cout << chocoSize << " " << divideCnt;
+
+}
